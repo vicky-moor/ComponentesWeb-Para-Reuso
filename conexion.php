21c64c919d89441761f47fb6d4218ba2efc69dbe
Miguel Ángel González <76235094+MikeGC-DEV@users.noreply.github.com>
2022-10-10 02:28:33 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";/*host*/
+    $user = "";/*usuario de base de datos */
+    $clave = "";/*clave de la base de datos */
+    $bd = "";/*nombre de la base de datos */
+    $conexion = mysqli_connect($host,$user,$clave,$bd);/*conexion con la base de datos y carga de datos */
+    if (mysqli_connect_errno()){
+        echo "No se pudo conectar a la base de datos";/*si no se conecta a la base de datos */
+        exit();
+    }
+    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+    mysqli_set_charset($conexion,"utf8");
+
+
+    
