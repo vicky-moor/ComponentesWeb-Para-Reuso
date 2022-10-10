43fd79d52071a8848489d0f2baae166ef914ac34
Miguel Ángel González <76235094+MikeGC-DEV@users.noreply.github.com>
2022-10-10 03:20:06 -0500

    Add files via upload

---
+++ productos.php
@@ -0,0 +1,138 @@
+<?php require_once "config/conexion.php"; ?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Importaciones Aldanaa</title>
+    <link rel="icon" href="img/unknown.png">
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <link href="assets/css/estilos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/menustyle.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+</head>
+
+<body>
+    <a href="ticket.html" class="btn-flotante">BOTÓN <span class="badge bg-success" id="">0</span></a>
+    <!-- Navigation-->
+    <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="user-menu">
+                        <ul>
+                            <li><a href=""><i class=""></i> BOTÓN</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--compañia area-->
+    
+    <div class="site-branding-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="logo">
+                        <h1><a href=""><img src="img/LOGO_EMPRESA.png" alt="" width="100px" height="100px"></a>'Nombre de la empresa'</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--compañia area fin-->
+    
+    <div id="menu1">
+        <ul>
+            <li><a href="">Apartado 1</a></li>
+            <li><a href="">Apartado 2</a></li>
+            <li><a href="">Apartado 3</a></li>
+        </ul>
+    </div>
+    
+    
+    <div class="slider-area">
+			<div class="block-slider block-slider4">
+				<ul class="" id="bxslider-home4">
+					<li>
+						<img src="img/SLIDE.png" alt="Slide">
+					</li>
+					<li>
+					    <img src="img/SLIDE.png" alt="Slide">
+					</li>
+					<li>
+					    <img src="img/SLIDE.png" alt="Slide">
+					</li>
+					<li>
+					    <img src="img/SLIDE.png" alt="Slide">
+					</li>
+				</ul>
+			</div>
+    </div> 
+    
+    
+   
+    
+    <div class="footer-top-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us">
+                        <h2><span>'Nombre de la empresa'</span></h2>
+                        <p>'Datos de la empresa'</p>
+                        
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                   
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-newsletter">
+                        <h2 class="footer-wid-title">'Información adicional'</h2>
+                        <p>'Información de contacto'</p>
+                        <div class="newsletter-form">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>&copy; 2022 Importaciones Aldanaa. All Rights Reserved. </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div> 
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bxslider.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.slider.js"></script>
+	
+</body>
+</html>
