1d9d087bb1e06a957f4f33048f072df1f1890099
Victoria Moreno <112165674+vicky-moor@users.noreply.github.com>
2022-10-10 23:06:28 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,9 +6,9 @@
     $conexion = mysqli_connect($host,$user,$clave,$bd);/*conexion con la base de datos y carga de datos */
     if (mysqli_connect_errno()){
         echo "No se pudo conectar a la base de datos";/*si no se conecta a la base de datos */
-        exit();
+        exit(); /*Salida*/
     }
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos"); /*si no se encuentra la base de datos */
     mysqli_set_charset($conexion,"utf8");
 
 
